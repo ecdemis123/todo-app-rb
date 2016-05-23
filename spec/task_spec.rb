d4b4ebed8476d966590670b5f5772e63230adbd2
Eleanor Demis <eleanordemis@gmail.com>
2016-05-22 17:36:54 -0700

    adds connection to db

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,10 +1,27 @@
 require('rspec')
 require('task')
+require('pg')
+
+DB = PG.connect({:dbname => 'to_do_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM tasks *;")
+  end
+end
 
 describe(Task) do
 
   before() do
     Task.clear()
+  end
+
+  describe("#==") do
+    it("is the same task if it has the same description") do
+      task1 = Task.new({:description => "learn SQL"})
+      task2 = Task.new({:description => "learn SQL"})
+      expect(task1).to(eq(task2))
+    end
   end
 
   describe("#description") do
